b2d36eb3b1edb76be2948dd1074c283b5de74b8c
Alex A.F <alex2andres@gmail.com>
2020-10-21 12:58:19 +0200

    Update Payment adding the credit card as a parameter for the request

--- src/Orders/Pay.php
+++ src/Orders/Pay.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RedsysRest\Orders;
 
+use RedsysRest\Common\CreditCard;
 use RedsysRest\Common\Currency;
 use RedsysRest\Common\Params;
 
@@ -12,7 +13,7 @@ class Pay implements Order
     private const TYPE = 0;
     private $amount;
     private $number;
-    private $card_number, $expiracy_date, $ccv2;
+    private $credit_card;
     private $tokenize = null;
     private $currency;
     private $merchant;
@@ -21,9 +22,7 @@ class Pay implements Order
     public function __construct(
         string $amount,
         string $number,
-        string $card_number,
-        string $expiracy_date,
-        string $ccv2,
+        CreditCard $credit_card,
         Currency $currency = null,
         string $merchant = null,
         string $terminal = null
@@ -31,9 +30,7 @@ class Pay implements Order
         $this->amount = $amount;
         $this->number = $number;
 
-        $this->card_number = $card_number;
-        $this->expiracy_date = $expiracy_date;
-        $this->ccv2 = $ccv2;
+        $this->credit_card = $credit_card;
 
         $this->currency = $currency;
         $this->merchant = $merchant;
@@ -48,9 +45,9 @@ class Pay implements Order
             Params::PARAM_CURRENCY => isset($this->currency) ? $this->currency->code() : null,
             Params::PARAM_MERCHANT => $this->merchant,
             Params::PARAM_TERMINAL => $this->terminal,
-            Params::PARAM_CARD_NUMBER => $this->card_number,
-            Params::PARAM_CARD_CVV2 => $this->ccv2,
-            Params::PARAM_CARD_EXPIRATION_DATE => $this->expiracy_date,
+            Params::PARAM_CARD_NUMBER => $this->credit_card->number(),
+            Params::PARAM_CARD_CVV2 => $this->credit_card->cvv2(),
+            Params::PARAM_CARD_EXPIRATION_DATE => $this->credit_card->expirationYear(). $this->credit_card->expirationMonth(),
             Params::PARAM_TRANSACTION_TYPE => self::TYPE,
         ];
         if ($this->tokenize != null) {
